cbfe3eaea74e2c135d36a73e21848274124441b9
winqihe <2450425958@qq.com>
2021-05-04 13:43:15 +0800

    tijiao

--- dish/hotel.sql
+++ dish/hotel.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80019
  File Encoding         : 65001
 
- Date: 25/04/2021 23:34:22
+ Date: 04/05/2021 13:41:58
 */
 
 SET NAMES utf8mb4;
@@ -34,24 +34,26 @@ CREATE TABLE `dish`  (
 -- ----------------------------
 -- Records of dish
 -- ----------------------------
-INSERT INTO `dish` VALUES (1, '香辣猪蹄', 46, '经典菜', 1291, '2021-04-25 19:32:00');
-INSERT INTO `dish` VALUES (2, '鱼香肉丝', 33, '特色菜', 634, '2021-04-25 19:33:00');
-INSERT INTO `dish` VALUES (3, '咸烧白', 27, '家常菜', 363, '2021-04-25 19:31:00');
-INSERT INTO `dish` VALUES (4, '麻婆豆腐', 19, '家常菜', 361, '2021-04-25 19:34:00');
-INSERT INTO `dish` VALUES (5, '青椒回锅肉', 32, '经典菜', 291, '2021-04-25 19:32:00');
-INSERT INTO `dish` VALUES (6, '鸡米芽菜', 22, '特色菜', 211, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (7, '宫保鸡丁', 44, '经典菜', 289, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (8, '酱香卤鸭', 53, '特色菜', 208, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (9, '红烧肉', 41, '家常菜', 177, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (10, '豆瓣鲜鱼', 30, '特色菜', 135, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (11, '冰淇凌', 3, '冷饮', 129, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (12, '咖啡', 3, '热饮', 107, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (13, '可乐', 2, '冷饮', 96, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (14, '绿茶', 2, '冷饮', 86, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (15, '奶茶', 5, '热饮', 80, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (16, '盖浇饭', 10, '主食', 69, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (17, '牛肉面', 9, '主食', 75, '2021/04/25,19:32:00');
-INSERT INTO `dish` VALUES (18, '番茄鸡蛋面', 8, '主食', 51, '2021/04/25,19:32:00');
+INSERT INTO `dish` VALUES (1, '香辣猪蹄', 46, '特辣', 1291, '2021-05-25 19:32:00');
+INSERT INTO `dish` VALUES (2, '鱼香肉丝', 33, '中辣', 634, '2021-05-25 19:32:33');
+INSERT INTO `dish` VALUES (3, '西红柿炒蛋', 27, '原味', 100, '2021-05-25 19:33:20');
+INSERT INTO `dish` VALUES (4, '麻婆豆腐', 24, '中辣', 361, '2021-05-25 19:34:04');
+INSERT INTO `dish` VALUES (5, '青椒回锅肉', 19, '微辣', 291, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (6, '水果沙拉', 22, '酸甜', 211, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (7, '苦瓜炒蛋', 44, '原味', 289, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (8, '火爆娃娃菜', 53, '原味', 208, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (9, '红烧肉', 41, '香甜', 177, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (10, '大盘鸡', 30, '微辣', 135, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (11, '冰淇凌', 3, '冷饮', 129, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (12, '咖啡', 3, '热饮', 107, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (13, '可乐', 2, '冷饮', 96, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (14, '绿茶', 2, '冷饮', 86, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (15, '奶茶', 5, '热饮', 80, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (16, '盖浇饭', 10, '原味', 69, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (17, '牛肉面', 9, '微辣', 75, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (18, '番茄鸡蛋面', 8, '原味', 51, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (19, '蚝油生菜', 17, '微辣', 99, '2021-05-25 19:34:34');
+INSERT INTO `dish` VALUES (20, '水煮西兰花', 12, '原味', 102, '2021-05-25 19:34:34');
 
 -- ----------------------------
 -- Table structure for health
